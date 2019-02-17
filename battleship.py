3eb3aa656beed155123e7890fdb3d5d679d63f99
Jake Herrmann <jtherrmann@alaska.edu>
2019-02-16 15:31:52 -0900

    Pass the test for aft on grid.

--- battleship.py
+++ battleship.py
@@ -15,7 +15,15 @@ class ShipOutOfGridError(IllegalPositionError):
 
 
 class Ship:
+
+    _lengths = {SUBMARINE: 3}
+
     def __init__(self, aft_location, direction, name):
         self._aft_location = aft_location
         self._direction = direction
         self._name = name
+        self._length = self._lengths[self._name]
+
+        for coordinate in self._aft_location:
+            if not 0 <= coordinate <= 9:
+                raise ShipOutOfGridError()
